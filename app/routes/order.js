b6aff3b85c733118ddf37a26db7927da0bf1a0fc
Mathis <mathisrouvers@gmail.com>
2022-03-25 16:34:21 +0100

    Avancée B : Order/idOrder

--- app/routes/order.js
+++ app/routes/order.js
@@ -1,7 +1,8 @@
-import Route from '@ember/routing/route';
+import Abstractroute from './AbstractRoute';
 
-export default class OrderRoute extends Route {
-  model() {
-    return{};
+export default class OrderRoute extends Abstractroute {
+  model(params) {
+    return this.store.findRecord('order', params.order_id, {
+      include: 'orderdetails.product,user'});
   }
 }
